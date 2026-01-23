e9ecdca9e310410b6d3476003726858e23804929
Farid Shahidi <farid.sh69@gmail.com>
2026-01-23 14:22:04 +0100

    adding eslint and prettier rules

---
+++ next-boilerplate/eslint.config.mjs
@@ -0,0 +1,32 @@
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import next from 'eslint-config-next'
+
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+  // Next.js rules
+  ...next,
+
+  // TypeScript overrides
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_', ignoreRestSiblings: true },
+      ],
+      'import/no-anonymous-default-export': 'off',
+    },
+  },
+]
---
+++ next-boilerplate/src/Test.ts
@@ -0,0 +1,3 @@
+export const X = 1
+
+const y = 1
